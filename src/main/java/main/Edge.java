f83265d584f322e66b96d618fbaece39b8a820d0
dipukr <adipukr@gmail.com>
2025-08-10 20:38:16 +0530

    first commit

---
+++ src/main/java/main/Edge.java
@@ -0,0 +1,14 @@
+package main;
+
+public class Edge {
+
+	public Node start;
+	public Node end;
+	public double cost;
+	
+	public Edge(Node start, Node end, double cost) {
+		this.start = start;
+		this.end = end;
+		this.cost = cost;
+	}
+}
---
+++ src/main/java/main/Search.java
@@ -0,0 +1,33 @@
+package main;
+
+import java.util.LinkedList;
+
+public class Search {
+
+	public void DFS(Node node) {
+		visit(node);
+		for (Node adjacentNode: node.adjacentNodes)
+			if (!adjacentNode.marked)
+				DFS(adjacentNode);
+	}
+
+	public void BFS(Node startNode) {
+		var queue = new LinkedList<Node>();
+		queue.offer(startNode);
+		visit(startNode);
+		while (!queue.isEmpty()) {
+			Node currNode = queue.poll();
+			for (Node adjacentNode: currNode.adjacentNodes) {
+				if (!adjacentNode.marked) {
+					visit(currNode);
+					queue.offer(currNode);
+				}
+			}	
+		}
+	}
+
+	public void visit(Node node) {
+		node.marked = true;
+		System.out.printf("%s ", node.data);
+	}
+}
