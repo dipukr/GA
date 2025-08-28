d4aaa03b6dd647f61c35fae39dd3c78e9eaf3c80
adipukr <dipu.kumar@50hertz.in>
2025-08-28 18:24:17 +0530

    delete files related to maze search

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -5,22 +5,6 @@ import java.util.Stack;
 
 public class Main {
 	public static void main(String[] args) throws Exception {
-		byte[][] data = {{1,6,7},{8,4,0},{5,2,3}};
-		State start = new State(data);
-		State goal = new State(State.goal);
-		Problem problem = new Puzzle(start, goal);
-		Map<String, State> prev = Solvers.aStar(problem);
-		Stack<State> states = new Stack<>();
-		states.add(problem.getGoalState());
-		while (!states.peek().equals(start))
-			states.add(prev.get(states.peek().toString()));
-		System.out.printf("Path size: %d\n", states.size());
-		byte[][][] frames = new byte[states.size()][][];
-		int count = 0;
-		while (!states.isEmpty())
-			frames[count++] = states.pop().data;
-		Anim.anim(frames);
-
 //		Graph graph = new Graph();
 //		Node a = Node.of("A");
 //		Node b = Node.of("B");
