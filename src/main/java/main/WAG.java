c66e8e92e1ac8c4a335550de244d192782e0c38c
dipukr <adipukr@gmail.com>
2025-09-27 10:49:43 +0530

    chg

--- src/main/java/main/WAG.java
+++ src/main/java/main/WAG.java
@@ -15,21 +15,17 @@ public class WAG {
 	public void DFS(int startNode, boolean[] visited, int[] prev) {
 		if (visited[startNode]) return;
 		visited[startNode] = true;
-		for (int i = 0; i < N; i++)
-			if (matrix[startNode][i] == 0.0 && !visited[i]) {
-				
+		for (int i = 0; i < N; i++) {
+			if (matrix[startNode][i] == 0.0 && !visited[i]) {		
 			}
+		}	
 	}
-	
 	
 	public void BFS(int startNode) {
 		var queue = new LinkedList<Integer>();
 		queue.offer(startNode);
-		
 		while (!queue.isEmpty()) {
 			int data = queue.poll();
-			
 		}
-				
 	}
 }
